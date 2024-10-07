448ba5d76a079552d06c5dc4f4b3f0207b9ef686
asom1989 <asom.1989@yahoo.com>
2024-10-07 16:14:30 +0200

    TEST

--- server.js
+++ server.js
@@ -27,6 +27,11 @@ app.get("/movies/:id", (req, res) => {
   const { id } = req.params;
   db.query("SELECT * FROM movie WHERE id = ?", [id], (err, results) => {
     if (err) return res.status(500).json(err);
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: "Movie not found" });
+    }
+
     res.json(results[0]);
   });
 });
